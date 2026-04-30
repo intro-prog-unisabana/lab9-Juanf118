9330393a7854846637c42782873db0ccafae3334
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-04-29 20:34:43 -0500

    song.py

--- song.py
+++ song.py
@@ -5,8 +5,8 @@ class Song:
         self.name = name
         self.artist = artist
         self.length = float(length)
-    #def get_length_in_seconds(self):
-        #return self.length * 60
+    def get_length_in_seconds(self):
+        return self.length * 60
     def __str__(self):
         return f"'{self.name}' by {self.artist} ({self.length})"
     
