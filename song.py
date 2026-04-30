4c3904bf18e8f18f8559db15125bca6563c5e54a
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-04-29 19:39:00 -0500

    song.py

--- song.py
+++ song.py
@@ -9,6 +9,8 @@ class Song:
         return self.length * 60
     def __str__(self):
         return f"{self.name} by {self.artist} ({self.length})"
+    def __repr__(self) -> str:
+        return f"Song(name={self.name!r}, artist={self.artist!r}, length={self.length!r})"
     
 if __name__ == "__main__":
     my_song = Song("tv off", "Kendrick Lamar", 3.7)
